79ce851145cbbfe39f380d02c176645b184145f2
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-07-11 19:54:28 +0500

    SETUP FOR TABS PROJECT

---
+++ tabs/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tabs from "./components/Tabs";
+
+function App() {
+  return (
+    <div>
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
---
+++ tabs/src/components/Tabs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Tabs() {
+  return (
+    <React.Fragment>
+      <div className="flex flex-col justify-center items-center space-y-2">
+        <div className="flex justify-center items-center space-x-3"></div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Tabs;
