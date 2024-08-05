ffa0e4a16a7206bd6eb2c32281c319e8c2597b98
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-08-05 10:18:23 +0500

    Error Solved

--- food-recipe-app/src/App.js
+++ food-recipe-app/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/Home/Home";
+import { Route, Routes } from "react-router";
+import FoodRecipe from "./components/food-recipe/FoodRecipe";
+import Favourite from "./components/favourite/Favourite";
+import Details from "./components/details/Details";
 function App() {
-  return <div className="flex justify-center items-center"></div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/foodRecipe" element={FoodRecipe} />
+        <Route path="/favourite" element={Favourite} />
+        <Route path="/recipe-item/:id" element={Details} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
